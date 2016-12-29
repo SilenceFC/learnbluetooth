340b2cc481efbf6959cf7929c734db1d6d142fd4
SilenceFC <1103451523@qq.com>
2016-12-29 10:54:40 +0800

    版本V1.0.1，详情请看readme.txt

---
+++ app/src/main/java/com/ut/learn/bluetooth/LaunchActivity.java
@@ -0,0 +1,39 @@
+package com.ut.learn.bluetooth;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+/**
+ * Created by admin on 2016/12/29.
+ */
+
+public class LaunchActivity extends Activity {
+    private Button bt_main,bt_second;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch);
+        initView();
+    }
+
+    private void initView() {
+        bt_main = (Button) findViewById(R.id.activity_main);
+        bt_second = (Button) findViewById(R.id.activity_second);
+    }
+
+    public void learn(View v){
+        switch (v.getId()){
+            case R.id.activity_main:
+                Intent intent = new Intent(LaunchActivity.this, MainActivity.class);
+                startActivity(intent);
+            break;
+            case R.id.activity_second:
+                Toast.makeText(LaunchActivity.this,"暂未开放，尽请期待~~~",Toast.LENGTH_LONG).show();
+                break;
+        }
+    }
+}
